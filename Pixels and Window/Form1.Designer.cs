678522f5a9c1abbf09ab34846b4f4f6081a56d89
ht6512yx <ht6512yx@T301026-1625307>
2016-10-18 20:48:29 -0500

    Change the background color to green

--- Pixels and Window/Form1.Designer.cs
+++ Pixels and Window/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(350, 338);
             this.Controls.Add(this.lblPixelslabel);
             this.Name = "Form1";
